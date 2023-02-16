e7e88d3e3f447d23d7cd118ce9164a3137cddb2b
jithi121 <jithi121@gmail.com>
2023-02-16 15:36:58 +0530

    things are coming back

--- changecases/content.js
+++ changecases/content.js
@@ -8,7 +8,19 @@ addEventListener("keydown", (event) => {
   }
 
 if(keys.a && keys.s){
-  console.log("both the keys pressed at once")
+  console.log("both the keys pressed at once");
 }
 
 });
+
+// browser.browserAction.onClicked.addListener(() => {
+//   browser.browserAction.setIcon({
+//     tabId: tab.id,
+//     path: "images/icons8-system-task-16.png",
+//     "16": "images/icons8-system-task-16-dark.png",
+//     "32": "images/icons8-system-task-32-dark.png",
+//     "48": "images/icons8-system-task-48-dark.png",
+//     "128": "images/icons8-system-task-64-dark.png"
+//   });
+// });
+
